424ee9a14c1c09a8f51a971662ba61f077ffd0cc
MichelleDelReal <michellealcantaradelreal@gmail.com>
2025-11-26 22:26:13 -0600

    Arreglando rutas duplicadas

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
-
-use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
+// Esta es la ruta automática para el usuario (si usas autenticación)
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// ESTA ES TU RUTA DE PELÍCULAS
+// Solo debe aparecer UNA vez.
 Route::apiResource('movies', MovieController::class);
+
+// Otras rutas API pueden ir aquí   
